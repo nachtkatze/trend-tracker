bf632aeaeccf675c57974ee293424f49d5159d4b
Óscar <oscar.aiborra@gmail.com>
2014-11-25 23:34:53 +0100

    Improved tweets proccessing and communication to JS

--- main.js
+++ main.js
@@ -1,30 +1,27 @@
 $(function() {
-	console.log("Ready!");
+    console.log("Ready!");
 
-	function AppViewModel() {
-		var self = this; 
-		self.recent_tweets = ko.observable();
+    function AppViewModel() {
+        var self = this;
+        self.recent_tweets = ko.observable();
 
-		var hashtags = '';
+        var loadTweets = function() {
+            tweets.push.apply(tweets, temp_tweets)
 
-		var loadTweets = function() {
-			tweets.push.apply(tweets, temp_tweets)
+            self.recent_tweets(tweets);
+        }
 
-			self.recent_tweets(tweets);
-		}
+        // var proccessHashtags = function() {
+        // 	console.log('Proccessing with a total of', totalCount, 'hashtags');
+        // }
 
-		var insertHashtag = function() {
-			
-		}
 
-		
-		loadTweets()
-		console.log("tweets", self.recent_tweets())
-		console.log(self.recent_tweets().length)
+        loadTweets()
+        console.log("tweets", self.recent_tweets())
+        console.log('Number of tweets:', self.recent_tweets().length)
 
-		var hashtags = [{text: "Hola", weigth:10}, {text: "Adios", weigth:8}, {text: "chato", weigth:5}, {text: "oly", weigth:1}]
-
-		$("#hashtag-cloud").jQCloud(hashtags, {delayedMode: true});
-	}
-	ko.applyBindings(new AppViewModel());
+        var hashtagsJS = jQuery.parseJSON(hashtags);
+        $("#hashtag-cloud").jQCloud(hashtagsJS);
+    }
+    ko.applyBindings(new AppViewModel());
 });
